fd6444a775572716f32d529e408378fbb9643924
P68222 <slowsnail@engineer.com>
2018-09-19 13:42:57 +0100

    removed space from plate

--- playground-files/creating-plates.py
+++ playground-files/creating-plates.py
@@ -1,5 +1,7 @@
-import random
+#!/usr/bin/python
+
 import string
+import random
 
 def numberplate():
     letters = string.ascii_uppercase
@@ -14,7 +16,7 @@ def numberplate():
         secondnumber = random.randrange(0,9)
     firstletters = ''.join([random.choice(minus_i) for n in range(0,2)])
     lastletters = ''.join([random.choice(minus_i) for n in range(0,3)])
-    print('{}{}{} {}'.format(firstletters, firstnumber, secondnumber, lastletters))
+    print('{}{}{}{}'.format(firstletters, firstnumber, secondnumber, lastletters))
 
 def numberplate1(size=7, chars=string.ascii_uppercase.replace("I","") + string.digits):
     plate = ''.join(random.choice(chars) for x in range(size))
@@ -26,9 +28,9 @@ def numberplate2():
     plate1 = ''.join(random.choice(totalletters) for x in range(2))
     plate2 = format(random.choice(totalrange),'02')
     plate4 = ''.join(random.choice(totalletters) for x in range(3))
-    print(plate1+plate2, plate4)
+    print(plate1+plate2+plate4)
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     numberplate()
     numberplate1()
     numberplate2()
